7c0cf7d3c480c41aa71e32b837bc3b73d21cf7dd
Mike Schinkel <mikeschinkel@gmail.com>
2012-11-30 00:44:36 -0500

    Added support for HTTP POST and added comment about need to support HTTP PUT and DELETE.

--- http-agents/wordpress-http-agent.php
+++ http-agents/wordpress-http-agent.php
@@ -1,5 +1,5 @@
 <?php
-
+// @todo: Need to support HTTP PUT AND DETEE
 /**
  * An HTTP Agent for RESTian that uses WordPress' WP_Http class and related functions to make HTTP requests.
  */
@@ -13,8 +13,20 @@ class RESTian_Wordpress_Http_Agent extends RESTian_Http_Agent_Base {
    */
   function make_request( $request, $response ) {
 
-    $result = wp_remote_get( $request->get_url(), $request->get_wp_args() );
-
+    switch ( $request->service->http_method ) {
+      case 'GET':
+        $result = wp_remote_get( $request->get_url(), $request->get_wp_args() );
+        break;
+      case 'POST':
+        $result = wp_remote_post( $request->get_url(), $request->get_wp_args() );
+        break;
+      case 'PUT':
+        $result = new WP_Error( -1, 'HTTP PUT not yet supported.' );
+        break;
+      case 'DELETE':
+        $result = new WP_Error( -2, 'HTTP DELETE not yet supported.' );
+        break;
+    }
     if ( is_wp_error( $result ) ) {
       /**
        * These errors likely won't be 100% compatible with the errors from CURL when standalone
